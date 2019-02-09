b778e8c2da897d35fb25ebe8b931ed85093574bd
Alex Hays <alexhays@gmail.com>
2019-02-08 22:33:08 -0500

    add some stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,14 @@ import React, { useState, useEffect, useRef } from 'react'
 import Layout from '../components/Layout'
 import ms from 'pretty-ms';
 
-import {  Grid, Heading, Box } from 'grommet'
+import {
+  Grid,
+  Heading,
+  Box,
+  Button,
+  TextArea,
+  TextInput,
+} from 'grommet'
 
 
 
@@ -26,13 +33,10 @@ function useInterval(callback, delay) {
   }, [delay]);
 }
 
-
-
-
 const IndexPage = () => {
-
   let [count, setCount] = useState(0);
-  let [delay, setDelay] = useState(1000);
+  let [delay, setDelay] = useState(300);
+  let [pastedText, setPastedText] = useState('');
 
   useInterval(() => {
     // Your custom logic here
@@ -40,30 +44,55 @@ const IndexPage = () => {
   }, delay);
 
   function handleDelayChange(e) {
-    setDelay(Number(e.target.value));
+    const input = Number(e.target.value)
+    if(isNaN(input) || input > 100000) return
+    setDelay(input)
   }
-
 
   return (
       <Layout>
         <Grid
           areas={[
             { name: 'header', start: [0, 0], end: [1, 0] },
-            { name: 'main', start: [0, 1], end: [1, 1] },
+            { name: 'settings', start: [0, 1], end: [1, 1] },
+            { name: 'btn-read', start: [0, 2], end: [1, 2] },
+            { name: 'main', start: [0, 3], end: [1, 3] },
           ]}
           columns={['small']}
-          rows={['xsmall', 'small']}
+          rows={['xsmall', 'xsmall', 'xxsmall','small']}
           gap='small'
         >
           <Box
-            a11yTitle
+            a11yTitle="Screen reader text"
             background='accent-1'
             gridArea='header'>
             <Heading alignSelf='center'>Speed Readr</Heading>
           </Box>
+          <Box
+            background='accent-3'
+            gridArea='settings'
+            fill
+            align="center"
+            justify="center"
+           >
+              <Box width="xsmall">
+                <TextInput
+                  value={delay}
+                  onChange={(e) => handleDelayChange(e) }
+                />
+              </Box>
+          </Box>
+          <Box gridArea='btn-read' background='accent-2'>
+             <Button alignSelf='center' label="Read"  onClick={() => {}} />
+          </Box>
           <Box gridArea='main' background='brand'>
             <h1>{count}</h1>
-            <input value={delay} onChange={handleDelayChange} />
+            <TextArea
+              fill={true}
+              value={pastedText}
+              placeholder='Paste text here'
+              onChange={(e) => setPastedText(e)}
+            />
           </Box>
         </Grid>
       </Layout>
